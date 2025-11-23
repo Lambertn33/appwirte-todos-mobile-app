2b072bc53b759c4539bc3af1205d3f30c59a2927
Lambert Nsengimana <lambertnsengimana@Lamberts-MacBook-Pro.local>
2025-11-23 23:32:56 +0200

    feat: add new private components

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,4 +1,5 @@
 import Authenticated from '@/components/auth/Authenticated';
+import { MaterialIcons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
 
@@ -6,7 +7,15 @@ const DashboardLayout = () => {
   return (
     <Authenticated>
         <Tabs>
-             <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+             <Tabs.Screen name="profile" options={{ title: "Profile", tabBarIcon: ({ color }) => (
+                <MaterialIcons name="person" color={color} size={28} />
+             ) }} />
+             <Tabs.Screen name="todos/createTodo" options={{ title: "Create Todo", tabBarIcon: ({ color }) => (
+                <MaterialIcons name="add" color={color} size={28} />
+             ) }} />
+             <Tabs.Screen name="todos/todosList" options={{ title: "Todos List", tabBarIcon: ({ color }) => (
+                <MaterialIcons name="list" color={color} size={28} />
+             ) }} />
         </Tabs>
     </Authenticated>
   )
---
+++ app/(dashboard)/todos/createTodo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CreateTodo = () => {
+  return (
+    <View>
+      <Text>CreateTodo</Text>
+    </View>
+  )
+}
+
+export default CreateTodo
+
+const styles = StyleSheet.create({})
